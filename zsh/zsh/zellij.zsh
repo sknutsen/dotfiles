cca53c1bbb92d56202e47039ffe3b9b57478b6ad
sknutsen <sondreknutsen1@gmail.com>
2025-01-03 20:26:38 +0100

    starship and alacritty
    - starship added
    - alacritty added
    - lazygit updated
    - zsh updated

--- zsh/zsh/zellij.zsh
+++ zsh/zsh/zellij.zsh
@@ -3,13 +3,13 @@ ZJ_SESSIONS=$(zellij list-sessions)
 NO_SESSIONS=$(echo "${ZJ_SESSIONS}" | wc -l)
 
 if [[ -z "$ZELLIJ" ]]; then
-    if [ "${NO_SESSIONS}" -e 1 ]; then
+    if [ "${NO_SESSIONS}" -eq 1 ]; then
         zellij attach -c
     else
         zellij
     fi
 
-    if [ "$ZELLIJ_AUTO_EXIT" == "true" ]; then
+    if [ "${ZELLIJ_AUTO_EXIT}" = "true" ]; then
         exit
     fi
 fi
