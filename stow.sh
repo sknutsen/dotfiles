ebe65f2db82624931326b46080e09c6bacd78adb
sknutsen <sondreknutsen1@gmail.com>
2025-07-27 16:36:15 +0200

    stow

--- stow.sh
+++ stow.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-stow ags
-stow alacritty
-stow bin
-stow git
-stow gtk
-stow hypr
-stow k9s
-stow kde
-stow kitty
-stow kvantum
-stow lazydocker
-stow lazygit
-stow nix
-stow nvim
-stow qt
-stow rofi
-stow starship
-stow swaync
-stow waybar
-stow zellij
-stow zsh
+stow --target="$HOME/.config/ags" ags
+stow --target="$HOME/.config/alacritty" alacritty
+stow --target="$HOME/.local/bin" bin
+stow --target="$HOME/" git
+stow --target="$HOME/.config" gtk
+stow --target="$HOME/.config/hypr" hypr
+stow --target="$HOME/.config/k9s" k9s
+stow --target="$HOME/.config/" kde
+stow --target="$HOME/.config/kitty" kitty
+stow --target="$HOME/.config/kvantum" kvantum
+stow --target="$HOME/.config/lazydocker" lazydocker
+stow --target="$HOME/.config/lazygit" lazygit
+stow --target="$HOME/" nvim
+stow --target="$HOME/.config" qt
+stow --target="$HOME/.config/rofi" rofi
+stow --target="$HOME/.config" starship
+stow --target="$HOME/.config/swaync" swaync
+stow --target="$HOME/.config/waybar" waybar
+stow --target="$HOME/.config/zellij" zellij
+stow --target="$HOME/" zsh
---
+++ unstow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+stow -D ags
+stow -D alacritty
+stow -D bin
+stow -D git
+stow -D gtk
+stow -D hypr
+stow -D k9s
+stow -D kde
+stow -D kitty
+stow -D kvantum
+stow -D lazydocker
+stow -D lazygit
+stow -D nix
+stow -D nvim
+stow -D qt
+stow -D rofi
+stow -D starship
+stow -D swaync
+stow -D waybar
+stow -D zellij
+stow -D zsh
